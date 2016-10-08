5958386b4a23c45a6523132ec3885dd9ac7683a4
Chris Etheridge <chris.etheridge@cognician.com>
2016-10-08 14:26:14 +0200

    Fixes and setup fn.

--- src/snake/core.clj
+++ src/snake/core.clj
@@ -1,36 +1,57 @@
 (ns cognician.server.s3
   (:require
-   [clojure.string :as string]
-   [cognician.config :as config]
-   [cognician.log :as log])
+   [clojure.string :as string])
   (:import
    [java.io ByteArrayInputStream]
    [com.amazonaws.auth BasicAWSCredentials InstanceProfileCredentialsProvider]
    [com.amazonaws.services.s3 AmazonS3Client]
-   [java.io File InputStream FileInputStream FileOutputStream Writer OutputStreamWriter]
-   [com.amazonaws.services.s3.model
-    ObjectMetadata S3Object PutObjectRequest CannedAccessControlList AmazonS3Exception
-    ListObjectsRequest]))
+   [java.io File InputStream FileInputStream FileOutputStream
+    Writer OutputStreamWriter]
+   [com.amazonaws.services.s3.model ObjectMetadata S3Object PutObjectRequest
+    CannedAccessControlList AmazonS3Exception ListObjectsRequest]))
 
 
-(def DEFAULT_BUCKET "")
+(def *s3-creds {:access-key nil
+                :secret-key nil})
 
 
-(defn s3-client []
-  (if (and ""
-           "")
-    (AmazonS3Client. (BasicAWSCredentials. "" ""))
+(defn setup!
+  "Sets up the S3 credentials map. This *must* be used if you are
+   not going to be using AWS instance profiles.
+
+   :access-key = S3 Access Key
+   :secret-key = S3 Secret key"
+  [^String access-key ^String secret-key]
+  (reset! *s3-creds{:access-key access-key
+                    :secret-key secret-key})
+  @*s3-creds)
+
+
+(defn s3-client
+  "Returns the current AWS S3 client."
+  []
+  (if (and (:access-key @*s3-creds)
+           (:secret-key @*s3-creds))
+    (AmazonS3Client. (BasicAWSCredentials. (:access-key @*s3-creds) (:secret-key @*s3-creds)))
     (AmazonS3Client. (InstanceProfileCredentialsProvider. true))))
 
 
 (defn copy-object
+  "Copies an object from `source bucket` to `destination bucket`.
+
+  If only supplied 1 bucket, the object will be copied to that bucket.
+
+  :src-bucket  = name of the source bucket
+  :src-key     = key of the source object
+  :dest-bucket = name of the destination bucket
+  :dest-key    = key of the destination object"
   ([bucket src-key dest-key]
    (copy-object bucket src-key bucket dest-key))
   ([src-bucket src-key dest-bucket dest-key]
    (.copyObject (s3-client) src-bucket src-key dest-bucket dest-key)))
 
 
-(defn object-summary->map [object-listing]
+(defn- object-summary->map [object-listing]
   (map (fn [summary]
          {:metadata {:content-length (.getSize summary)
                      :etag           (.getETag summary)
